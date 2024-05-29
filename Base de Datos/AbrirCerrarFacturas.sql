d7a14017b9c53ff72afdac83c6b1ca1cd3895f58
stephsandoval <stephsandovalc@gmail.com>
2024-05-29 16:51:00 -0600

    comentarios y documentacion

--- Base de Datos/AbrirCerrarFacturas.sql
+++ Base de Datos/AbrirCerrarFacturas.sql
@@ -1,13 +1,58 @@
+-- Armando Castro, Stephanie Sandoval | Jun 10. 24
+-- Tarea Programada 03 | Base de Datos I
+
+-- Procedimiento:
+-- ABRE Y CIERRA FACTURAS SEGUN FECHA DE OPERACION
+
+-- Descripcion general:
+-- Por cada dia de operacion se deben procesar contratos que cierran y abren facturas
+-- Cerrar una factura implica consolidar los montos finales que el usuario debe pagar por el mes
+-- Abrir una factura implica abrir una nueva instancia para comenzar a sumar los montos del mes
+-- Se abre y cierra factura con base en el dia en que se firma el contrato
+	-- Por ejemplo, un contrato firmado el 30 de enero, cierra los dias 30
+	-- Se toma cierta precaucion para meses que tienen menos de 30 dias (febrero)
+	-- En dicho mes, la factura cerraria el dia 28 o 29 segun el tipo de año
+	-- Una logica similar se aplica para los demas dias
+-- Abrir una factura tambien implica abrir un nuevo detalle e instancias de cobro fijo
+
+-- Descripcion de parametros:
+	-- @inFechaOperacion: fecha en la cual se esta ejecutando el procedimiento
+	-- @outResultCode: resultado de ejecucion del codigo
+		-- si el codigo es 0, el codigo se ejecuto correctamente
+		-- si es otro valor, ocurrio un error
+
+-- Ejemplo de ejecucion:
+	-- DECLARE @outResultCode INT
+	-- EXECUTE dbo.AbrirCerrarFacturas 'yyyy-mm-dd', @outResultCode OUTPUT
+
+-- Notas adicionales:
+-- el SP se apoya de funciones adicionales para realizar algunos calculos
+-- estas se encargan de:
+	-- encontrar los contratos a los que se les debe cerrar una factura
+	-- calcular los montos finales para las facturas que cierran
+	-- generar las fechas de cierre y de pago
+
+-- por otro lado, para las facturas que abren, se les suma la tarifa base
+-- esto porque, independientemente de lo que pase, eventualmente se les tiene que sumar
+-- resulta mas facil hacerlo justo cuando se abren
+
+-- finalmente, cabe notar que tambien se abren las instancias en la tabla CobroFijo
+-- esto para aprovechar los IDs que se pueden recolectar aqui
+-- (luego de haber generado la instancia en Detalle propiamente)
+
+-- ************************************************************* --
+
 ALTER PROCEDURE dbo.AbrirCerrarFacturas
-      @inFechaOperacion DATE
-    , @outResultCode INT OUTPUT
+      @inFechaOperacion DATE                                     -- fecha en que se ejecuta el SP
+    , @outResultCode INT OUTPUT                                  -- resultado de la ejecucion del SP
 AS
 BEGIN
     SET NOCOUNT ON;
     BEGIN TRY
 
         -- DECLARAR VARIABLES:
-        DECLARE @ClienteCierre TABLE (
+
+        DECLARE @ClienteCierre TABLE (                           -- tabla para los contratos que cierran factura
               SEC INT IDENTITY(1,1)
             , IDContrato INT
 			, MontoAntesIVA MONEY
@@ -16,25 +61,34 @@ BEGIN
 			, MontoTotal MONEY
         );
 
-		DECLARE @ClienteApertura TABLE (
+		DECLARE @ClienteApertura TABLE (                         -- tabla para los contratos que abren facturas
 			  SEC INT IDENTITY(1,1)
 			, IDContrato INT
 		);
 
-		DECLARE @NuevaFactura TABLE (
+		DECLARE @NuevaFactura TABLE (                            -- tabla para los IDs de las facturas que se generan
               IDFactura INT
             , IDContrato INT
         );
 
-		DECLARE @NuevoDetalle TABLE (
+		DECLARE @NuevoDetalle TABLE (                            -- tabla para los IDs de los detalles que se generan
 			  IDDetalle INT
 			, IDFactura INT
 		);
 
+		-- ------------------------------------------------------------- --
         -- INICIALIZAR VARIABLES:
+
         SET @outResultCode = 0;
 
+		-- ------------------------------------------------------------- --
         -- INICIALIZAR TABLAS:
+
+		-- ingresar la informacion de los contratos que cierran factura
+		-- se apoya de dos funciones, las cuales:
+			-- 1. determina los IDs de los contratos
+			-- 2. calcula los montos finales para la factura de un contrato especifico
+
 		INSERT INTO @ClienteCierre (
 			  IDContrato
 			, MontoAntesIVA
@@ -50,18 +104,27 @@ BEGIN
 		FROM dbo.ObtenerContratosCierre (@inFechaOperacion) CC
 		CROSS APPLY dbo.CalcularMontosFinalesFactura (CC.IDContrato, @inFechaOperacion) MT;
 
+		-- ingresar la informacion de los contratos que abren factura
+		-- estos son aquellos que:
+			-- recien firmaron contrato el dia de operacion
+			-- cierran factura el dia de operacion
+
 		INSERT INTO @ClienteApertura (IDContrato)
 		SELECT C.ID
 		FROM dbo.Contrato C
 		WHERE C.FechaContrato = @inFechaOperacion;
 
 		INSERT INTO @ClienteApertura (IDContrato)
-		SELECT IDContrato
-		FROM dbo.ObtenerContratosCierre (@inFechaOperacion);
+		SELECT CC.IDContrato
+		FROM dbo.ObtenerContratosCierre (@inFechaOperacion) CC;
+
+		-- ------------------------------------------------------------- --
+		-- ABRIR Y CERRAR FACTURAS:
 
 		BEGIN TRANSACTION tOperarFactura
 
 			-- cerrar facturas:
+
 			UPDATE F
 			SET 
 				TotalPagarAntesIVA = CC.MontoAntesIVA,
@@ -72,7 +135,10 @@ BEGIN
 			INNER JOIN @ClienteCierre CC ON F.IDContrato = CC.IDContrato
 			WHERE F.FechaFactura = @inFechaOperacion;
 
+			-- ------------------------------------------------ --
 			-- abrir nuevas facturas:
+				-- utiliza el OUTPUT para almacenar los IDs de las facturas que se generan
+
 			INSERT INTO dbo.Factura (IDContrato
 				, TotalPagarAntesIVA
 				, TotalPagarDespuesIVA
@@ -84,10 +150,10 @@ BEGIN
 			)
 			OUTPUT INSERTED.ID, INSERTED.IDContrato INTO @NuevaFactura (IDFactura, IDContrato)
 			SELECT CA.IDContrato
-				, CASE 
+				, CASE                                           -- agregar el monto base de la tarifa
 					WHEN ETT.Valor = NULL THEN 0 
 					WHEN ETT.IDTipoElemento = 9 OR ETT.IDTipoElemento = 10 THEN 0
-					ELSE ETT.VALOR 
+					ELSE ETT.VALOR
 				  END
 				, 0
 				, 0
@@ -101,10 +167,19 @@ BEGIN
 			WHERE ETT.IDTipoElemento = 1 OR ETT.IDTipoElemento = 9 OR ETT.IDTipoElemento = 10
 			ORDER BY CA.SEC
 
+			-- ------------------------------------------------ --
+			-- abrir nuevos detalles:
+				-- utiliza los IDs capturados anteriormente para establecer las FKs
+				-- utiliza el OUTPUT para almacenar los IDs de los detalles generados
+
 			INSERT INTO dbo.Detalle (IDFactura)
-			OUTPUT Inserted.ID, Inserted.IDFactura INTO @NuevoDetalle (IDDetalle, IDFactura) 
+			OUTPUT INSERTED.ID, INSERTED.IDFactura INTO @NuevoDetalle (IDDetalle, IDFactura) 
             SELECT NF.IDFactura
             FROM @NuevaFactura NF;
+
+			-- ------------------------------------------------ --
+			-- abrir nuevas instancias de cobro fijo:
+				-- utiliza los IDs capturados anteriormente para establecer las FKs
 
 			INSERT INTO dbo.CobroFijo (IDDetalle
 				, IDElementoDeTipoTarifa)
@@ -118,6 +193,8 @@ BEGIN
             WHERE TE.EsObligatorio = 1;
 
 		COMMIT TRANSACTION tOperarFactura
+
+		-- ------------------------------------------------------------- --
 
         SELECT @outResultCode AS outResultCode;
 
@@ -144,3 +221,6 @@ BEGIN
     END CATCH;
     SET NOCOUNT OFF;
 END;
+
+-- ************************************************************* --
+-- fin del SP para abrir y cerrar facturas
--- Base de Datos/CalcularMontoLlamadas.sql
+++ Base de Datos/CalcularMontoLlamadas.sql
@@ -1,3 +1,39 @@
+-- Armando Castro, Stephanie Sandoval | Jun 10. 24
+-- Tarea Programada 03 | Base de Datos I
+
+-- Funcion:
+-- CALCULA LOS MONTOS POR LAS LLAMADAS REALIZADAS
+
+-- Descripcion general:
+-- Para las facturas que cierran, se deben calcular los montos por llamadas realizadas
+-- Cada contrato esta asociado con un tipo de tarifa, el cual tiene una cantidad de minutos base
+-- Si la cantidad de minutos utilizados no sobrepasa el minimo, no se le cobra extras
+
+-- De lo contrario, se siguen las siguientes especificaciones:
+	-- Si la llamada sucede en horario diurno, cada minuto se cobra por una tarifa adicional
+	-- Si la llamada sucede en horario nocturno, cada minuto se cobra por una tarifa adicional reducida
+		-- Se considera horario nocturno al lapso entre 23 PM y 05 AM
+	-- El horario de la llamada se determina segun su hora de fin
+
+-- Las llamadas que se cobran segun los montos de las tarifas son las locales
+-- Es decir, van hacia numeros 8XXX-XXXX
+-- Si son numeros 6XXX-XXX, 7XXX-XXX, 800-XXXX-XXXX o 900-XXX-XXX se cobran diferente
+
+-- Descripcion de parametros:
+	-- @inIDContrato: ID del contrato para el que se calculan los montos finales
+	-- @inFechaOperacion: fecha en la cual se esta ejecutando el procedimiento
+	-- retona INT indicando el monto total por llamadas
+
+-- Ejemplo de ejecucion:
+	-- SELECT dbo.CalcularMontoLlamadas (0, 'yyyy-mm-dd')
+
+-- Notas adicionales:
+-- la funcion calcula el monto que se le debe sumar a la tarifa base
+-- el monto de la tarifa base ya se sumo previamente
+	-- (durante la apertura de la factura)
+
+-- ************************************************************* --
+
 ALTER FUNCTION dbo.CalcularMontoLlamadas (
 	  @inIDContrato INT
 	, @inFechaOperacion DATE
@@ -8,26 +44,29 @@ BEGIN
 
 	-- DECLARAR VARIABLES:
 
-	DECLARE @numeroTelefono VARCHAR(32);
-	DECLARE @IDDetalle INT;
+	DECLARE @numeroTelefono VARCHAR(32);                         -- numero de telefono del contrato
+	DECLARE @IDDetalle INT;                                      -- numero del detalle asociado a la factura actual
 
-	DECLARE @monto800 INT;
-	DECLARE @cantidadLlamadasRecibidas INT;
+	DECLARE @monto800 INT;                                       -- monto por llamadas 800
+	DECLARE @cantidadLlamadasRecibidas INT;                      -- cantidad de llamadas recibidas por un numero 800
 	
-	DECLARE @cantidadLlamada900 INT;
-	DECLARE @monto900 MONEY;
-	DECLARE @cantidadLlamadas INT;
-	DECLARE @llamadaActual INT;
-	DECLARE @cantidadActualMinutos INT;
-	DECLARE @tarifaNocturno MONEY;
-	DECLARE @tarifaDiurno MONEY;
-	DECLARE @cantidadMinutos INT;
-	DECLARE @cantidadMinutosBase INT;
-	DECLARE @horaFin DATETIME;
+	DECLARE @monto900 MONEY;                                     -- monto por llamadas 900
+	DECLARE @cantidadLlamada900 INT;                             -- cantidad de llamadas realizadas a numero 900
+	
+	DECLARE @tarifaNocturno MONEY;                               -- monto del horario nocturno segun tarifa
+	DECLARE @tarifaDiurno MONEY;                                 -- monto del horario diurno segun tarifa
 
-	DECLARE @montoTotal INT;
+	DECLARE @horaFin DATETIME;                                   -- hora en que termina la llamada
+	DECLARE @cantidadLlamadas INT;                               -- cantidad de llamadas locales realizadas
+	DECLARE @llamadaActual INT;                                  -- variable para iterar sobre las llamadas
+	DECLARE @cantidadActualMinutos INT;                          -- cantidad de minutos que se han procesado
+	DECLARE @cantidadMinutos INT;                                -- cantidad de minutos de la llamada actual
+	DECLARE @cantidadMinutosBase INT;                            -- cantidad de minutos de la tarifa
 
-	DECLARE @LlamadaRegistradaZ TABLE (
+	DECLARE @montoTotal INT;                                     -- monto total por llamadas
+
+	-- tabla para registrar las llamadas hacia la empresa Z:
+	DECLARE @LlamadaRegistradaZ TABLE (                          -- tabla para las llamadas locales
 		  SEC INT IDENTITY(1,1)
 		, HoraFin DATETIME
 		, CantidadMinutos INT
@@ -36,12 +75,12 @@ BEGIN
 	-- ------------------------------------------------------------- --
 	-- INICIALIZAR VARIABLES:
 
-	-- identificar el numero de telefono que se esta revisando
+	-- identificar el numero de telefono que se esta revisando:
 	SELECT @numeroTelefono = C.NumeroTelefono
 	FROM dbo.Contrato C
 	WHERE C.ID = @inIDContrato
 
-	-- obtener el id del detalle asociado con el numero para la factura actual
+	-- obtener el id del detalle asociado con el numero para la factura actual:
 	SELECT @IDDetalle = D.ID
 	FROM Detalle D
 	INNER JOIN Factura F ON D.IDFactura = F.ID
@@ -50,66 +89,67 @@ BEGIN
 	-- ------------------------------------------------------------- --
 	-- CALCULAR MONTOS:
 
-	-- si el numero es de tipo 800 (paga todas las llamadas, recibidas y hechas)
+	-- si el numero es de tipo 800 (paga todas las llamadas, recibidas y hechas):
 	IF (@numeroTelefono LIKE '800%')
 	BEGIN
-		-- encontrar el monto para las llamadas 800
+		-- encontrar el monto para las llamadas 800:
 		SELECT @monto800 = ETT.Valor
 		FROM dbo.ElementoDeTipoTarifa ETT
 		WHERE ETT.IDTipoTarifa = 7 AND ETT.IDTipoElemento = 9
 
-		-- encontrar la cantidad de llamadas asociadas al numero
+		-- encontrar la cantidad de llamadas asociadas al numero:
 		SELECT @cantidadLlamadasRecibidas = ISNULL(SUM(L.CantidadMinutos), 0)
 		FROM dbo.Llamada L 
 		WHERE L.IDDetalle = @IDDetalle;
 
-		-- calcular el monto total para las llamadas
+		-- calcular el monto total para las llamadas:
 		SET @montoTotal = @monto800 * @cantidadLlamadasRecibidas;
 	END
 	ELSE
 	-- si el numero no es de tipo 800
 	BEGIN
 		-- PARA LLAMADAS LOCALES:
-		-- 1. obtener valores para calcular los montos por llamadas a 90
+		-- 1. obtener valores para calcular los montos por llamadas a 900:
 
-		-- obtener la cantidad de llamadas a numero 900
+		-- obtener la cantidad de llamadas a numero 900:
 		SELECT @cantidadLlamada900 = COUNT(L.ID)
 		FROM dbo.Llamada L
 		INNER JOIN dbo.LlamadaInput LI ON LI.ID = L.IDLlamadaInput
 		WHERE L.IDDetalle = @IDDetalle AND LI.NumeroA LIKE '900%'
 
-		-- obtener el monto para las llamadas hacia numero 900
+		-- obtener el monto para las llamadas hacia numero 900:
 		SELECT @monto900 = ETT.Valor
 		FROM dbo.ElementoDeTipoTarifa ETT
 		WHERE ETT.IDTipoElemento = 10 AND ETT.IDTipoTarifa = 8
 
-		-- agregar el monto por llamadas a 900 al monto total
+		-- agregar el monto por llamadas a 900 al monto total:
 		SET @montoTotal = @cantidadLlamada900 * @monto900
 
 		-- ---------------------------------------- --
-		-- 2. obtener valores para calcular los montos por llamadas a 8XXX-XXXX
+		-- 2. obtener valores para calcular los montos por llamadas a 8XXX-XXXX:
 
-		-- obtener la tarifa para horario diurno segun tipo de tarifa
+		-- obtener la tarifa para horario diurno segun tipo de tarifa:
 		SELECT @tarifaDiurno = ETT.Valor
 		FROM dbo.ElementoDeTipoTarifa ETT
 		INNER JOIN Contrato C ON C.IDTipoTarifa = ETT.IDTipoTarifa
 		WHERE C.ID = @inIDContrato AND ETT.IDTipoElemento = 3
 
-		-- obtener la tarifa para horario nocturno segun tipo de tarifa
+		-- obtener la tarifa para horario nocturno segun tipo de tarifa:
 		SELECT @tarifaNocturno = ETT.Valor
 		FROM dbo.ElementoDeTipoTarifa ETT
 		INNER JOIN Contrato C ON C.IDTipoTarifa = ETT.IDTipoTarifa
 		WHERE C.ID = @inIDContrato AND ETT.IDTipoElemento = 4
 
-		-- obtener la cantidad de minutos base
+		-- obtener la cantidad de minutos base:
 		SELECT @cantidadMinutosBase = ETT.Valor
 		FROM dbo.ElementoDeTipoTarifa ETT
 		INNER JOIN Contrato C ON C.IDTipoTarifa = ETT.IDTipoTarifa
 		WHERE C.ID = @inIDContrato AND ETT.IDTipoElemento = 2
 
 		-- ---------------------------------------- --
-	 
-		-- obtener la cantidad de llamadas locales realizadas
+	 	-- 3. obtener la cantidad de llamadas locales realizadas:
+
+		-- cantidad de llamadas realizadas a numeros 8XXX-XXXX:
 		INSERT INTO @LlamadaRegistradaZ (
 			  HoraFin
 			, CantidadMinutos
@@ -121,45 +161,46 @@ BEGIN
 		INNER JOIN dbo.LlamadaLocal LL ON LL.IDLlamada = L.ID
 		WHERE L.IDDetalle = @IDDetalle
 
-		-- obtener la cantidad de llamadas realizadas para poder iterar
+		-- obtener la cantidad de llamadas realizadas para poder iterar:
 		SELECT @cantidadLlamadas = COUNT(SEC) FROM @LlamadaRegistradaZ
 
 		-- ---------------------------------------- --
+		-- 4. calcular el monto final segun la cantidad de minutos sobre el maximo
 
 		SET @llamadaActual = 1
 		SET @cantidadActualMinutos = 0;
 
-		-- mientras no se hayan procesado todas las llamadas
+		-- mientras no se hayan procesado todas las llamadas:
 		WHILE @llamadaActual <= @cantidadLlamadas
 		BEGIN
-			-- obtener la cantidad de minutos que duro la llamada actual
+			-- obtener la cantidad de minutos que duro la llamada actual:
 			SELECT @cantidadMinutos = LRZ.CantidadMinutos
 			FROM @LlamadaRegistradaZ LRZ
 			WHERE LRZ.SEC = @llamadaActual
 
-			-- si al sumar la cantidad de minutos se pasa de los minutos base
+			-- si al sumar la cantidad de minutos se pasa de los minutos base:
 			IF (@cantidadActualMinutos + @cantidadMinutos > @cantidadMinutosBase)
 			BEGIN
-				-- determinar el horario de la llamada
+				-- determinar el horario de la llamada:
 				SELECT @horaFin = LRZ.HoraFin
 				FROM @LlamadaRegistradaZ LRZ
 				WHERE LRZ.SEC = @llamadaActual
 
-				-- si la llamada sucede en horario nocturno (aplica tarifa nocturna)
+				-- si la llamada sucede en horario nocturno (aplica tarifa nocturna):
 				IF (DATEPART(HOUR, @horaFin) >= 23 OR DATEPART(HOUR, @horaFin) < 5)
 				BEGIN
-					-- agregar al monto total el cobro por los minutos adicionales segun tarifa nocturna
+					-- agregar al monto total el cobro por los minutos adicionales segun tarifa nocturna:
 					SET @montoTotal = @montoTotal + (@cantidadActualMinutos + @cantidadMinutos - @cantidadMinutosBase) * @tarifaNocturno;
 				END
 				ELSE
 				-- si la llamada sucede en horario diurno
 				BEGIN
-					-- agregar al monto total el cobro por los minutos adicionales segun tarifa diurna
+					-- agregar al monto total el cobro por los minutos adicionales segun tarifa diurna:
 					SET @montoTotal = @montoTotal + (@cantidadActualMinutos + @cantidadMinutos - @cantidadMinutosBase) * @tarifaDiurno;
 				END
 			END
 		
-			-- sumar los minutos de la llamada a la cantidad de minutos que se han registrado
+			-- sumar los minutos de la llamada a la cantidad de minutos que se han registrado:
 			SET @cantidadActualMinutos = @cantidadActualMinutos + @cantidadMinutos
 			SET @llamadaActual = @llamadaActual + 1
 		END;
@@ -167,3 +208,6 @@ BEGIN
 
 	RETURN @montoTotal;
 END
+
+-- ************************************************************* --
+-- fin del SP para abrir y cerrar facturas
