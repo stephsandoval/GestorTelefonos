de3aeb8120b16cbd5852d320166f40d6808648a6
nesscafe <40587996+nesscoffee@users.noreply.github.com>
2024-06-03 22:37:39 -0600

    Setup Spring Boot

---
+++ CapaLogica/src/main/java/gestorTelefonos/CapaLogicaApplication.java
@@ -0,0 +1,13 @@
+package gestorTelefonos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapaLogicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapaLogicaApplication.class, args);
+	}
+
+}
---
+++ CapaLogica/src/test/java/com/example/gestor/CapaLogicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapaLogicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
