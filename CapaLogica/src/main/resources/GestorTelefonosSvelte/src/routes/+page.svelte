e7034958f82915bb55cb13cf2108fd4027242096
nesscafe <40587996+nesscoffee@users.noreply.github.com>
2024-06-05 17:49:17 -0600

    Creacion Pantalla Main Svelte

--- CapaLogica/src/main/resources/GestorTelefonosSvelte/src/routes/+page.svelte
+++ CapaLogica/src/main/resources/GestorTelefonosSvelte/src/routes/+page.svelte
@@ -1,1 +1,88 @@
-<h1>Svelte</h1>
+<script>
+
+    let inputTelefono;
+    let inputEmpresa;
+    let boolInputTelefono = false;
+    let boolInputEmpresa = false;
+
+    const showInputTelefono = () => {
+        boolInputTelefono = true;
+        boolInputEmpresa = false;
+    }
+
+    const showInputEmpresa = () => {
+        boolInputEmpresa = true;
+        boolInputTelefono = false;
+    }
+
+    const consultarTelefono = () => { 
+        //...
+    }
+
+    const consultarEmpresa = () => {
+        //...
+    }
+</script>
+
+<div class="main">
+    <h1>Facturador de servicios telefónicos</h1>
+    <div class="botones">
+        <button on:click={showInputTelefono}>Consultar número de teléfono</button>
+        <div style="display: {boolInputTelefono ? "block" : "none"};">
+            <input type="text" 
+            name="inputTelefono" id="inputTelefono" 
+            placeholder="Teléfono móvil"
+            bind:value={inputTelefono}
+            on:input={
+                () => {
+                    const regex = /[0-9]+/i;
+                    if(regex.test(inputTelefono)) {
+                        document.getElementById("inputTelefono").style.border = "";
+                    } else {
+                        document.getElementById("inputTelefono").style.border = "2px solid red";
+                    }
+                }
+            }>
+            <button on:click={consultarTelefono}>Procesar consulta</button>
+        </div>
+        <button on:click={showInputEmpresa}>Consultar estado de cuenta de empresa</button>
+        <div style="display: {boolInputEmpresa ? "block" : "none"};">
+            <input type="text" 
+            name="inputEmpresa" id="inputEmpresa" 
+            placeholder="Nombre de empresa"
+            bind:value={inputEmpresa}>
+            <button on:click={consultarEmpresa}>Procesar consulta</button>
+        </div>
+    </div>
+</div>
+
+<style>
+    :global(body) {
+        background-color: #DDE6ED;
+    }
+
+    h1 {
+        text-align: center;
+        font-family: 'Trebuchet MS';
+        font-style: italic;
+    }
+
+    .botones {
+        display: grid;
+        justify-content: center;
+    }
+
+    .botones button {
+        background-color: #9DB2BF;
+        font-size: 16px;
+        border-radius: 4px;
+        margin: 15px;
+        padding: 5px 5px;
+        transition-duration: 0.4s;
+        cursor: pointer;
+    }
+
+    .botones button:hover {
+        background-color: #6f7e87;
+    }
+</style>
